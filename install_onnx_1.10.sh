fb5b87c7ee5535e620ce04c76b1d886b1243b518
yuzhangkai <53356318+yuzhangkaii@users.noreply.github.com>
2024-07-07 21:00:29 +0800

    Add files via upload

--- install_onnx_1.10.sh
+++ install_onnx_1.10.sh
@@ -1,3 +1,3 @@
-wget https://github.com/yuzhangkaii/build_test_yolov5/raw/main/onnxruntime.zip -O ~/onnxruntime.zip
+wget gitdl.cn/https://github.com/yuzhangkaii/build_test_yolov5/raw/main/onnxruntime.zip -O ~/onnxruntime.zip
 unzip onnxruntime.zip -d /usr/local/src
 rm -fr ~/onnxruntime.zip
